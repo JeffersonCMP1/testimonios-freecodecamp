8ff4ee3d550e5665f9c70344f1c9b98884597634
JeffersonXd <196490824+JeffersonCMP1@users.noreply.github.com>
2026-05-05 01:23:39 -0500

    anadir componente testimonio

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Testimonio from './componentes/Testimonio';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+      <Testimonio />
+    </div>
+  );
+}
+
+export default App;
