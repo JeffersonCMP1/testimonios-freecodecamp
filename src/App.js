5cfba87a45952ff71df437d0253e751165d0c12c
JeffersonXd <196490824+JeffersonCMP1@users.noreply.github.com>
2026-05-09 21:17:56 -0500

    agregar mas testimonios, asi mismo se anadio las imagenes

--- src/App.js
+++ src/App.js
@@ -9,11 +9,29 @@ function App() {
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
         <Testimonio 
         nombre='Shawn Wang'
-        image=''
         pais='Singapore'
+        image='Shawn'
         cargo='Ingeniero de Software'
         empresa='Amazon'
         testimonio='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software.'
+        />
+
+        <Testimonio 
+        nombre='Sarah Chima'
+        pais='Nigeria'
+        image='Sarah'
+        cargo='Ingeniero de Software'
+        empresa='ChatDesk'
+        testimonio='freeCodeCamp fue la puerta de entrada a mi carrera como desarrolladora de software. El plan de estudios bien estructurado llevó mis conocimientos de codificación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrolladora en una empresa increíble.'
+        />
+
+           <Testimonio 
+        nombre='Emma Bostian'
+        pais='Sweden'
+        image='Emma'
+        cargo='Ingeniero de Software'
+        empresa='Spotify'
+        testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniera de software en Spotify.'
         />
       </div>
     </div>
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -5,12 +5,13 @@ import '../hojas-de-estilo/Testimonio.css';
 export default function Testimonio(props){
     return(
     <div className="contenedor-testimonio">
-        <img className='imagen-testimonio' src={require('../imagenes/persona1.png')}alt="Persona 1" />
+        <img className='imagen-testimonio' src={require(`../imagenes/${props.image}.png`)}alt="Persona 1" />
         <div className='contenedor-texto-testimonio'>
-            <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
-            <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
+            <p className='nombre-testimonio'> <strong> {props.nombre}</strong> en {props.pais}</p>
+            <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
             <p className='texto-testimonio'>{props.testimonio}</p>
         </div>
     </div>
     )
 }
+
